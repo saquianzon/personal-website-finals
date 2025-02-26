e928bc4df96c07a5236c65d3a371a802d372e8c0
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-02-26 15:05:39 +0800

    vvvv

--- firstsfc/src/components/CommentForm.vue
+++ firstsfc/src/components/CommentForm.vue
@@ -114,7 +114,7 @@ async function submitComment() {
   width: 100%;
   border: none;
   border-bottom: 2px solid #ccc;
-  padding: 80px;
+  padding: 10px;
   background: #fff;
   box-sizing: border-box;
   border-radius: 5px;
