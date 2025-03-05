1ef86e48c3cdd8d8b74217124cc058f11f9437ac
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-03-05 18:48:58 +0800

    updated css

--- firstsfc/src/components/Comment.vue
+++ firstsfc/src/components/Comment.vue
@@ -117,7 +117,7 @@ main {
 
 @media (min-width: 1024px) {
   .comment-container {
-    margin-left: 300px
+    margin-left: 350px
   }
 }
 
@@ -129,7 +129,7 @@ main {
 
 @media (min-width: 768px) {
   .comment-container {
-    margin-left: 290px
+    margin-left: 320px
   }
 }
 
